1235358fa5d9c9968e9210da794f2e9970f32dab
Vasanth-Pippari <pipparivasanth66700@gmail.com>
2024-05-21 20:47:11 +0530

    portals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState} from 'react'
+import './App.css'
+import ReactDOM from 'react-dom'
+function Box(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    ReactDOM.createPortal(
+    <>
+    <div className='model-overlay'>
+      <div className='content'>
+        <h2>This is introduction .Please be listen carefulluy</h2>
+        <p>The time u get as the best prove to the world extra so that everyone can get to know</p>
+        <button onClick={props.close}>Close</button>
+
+      </div>
     </div>
-  );
+
+  </>,document.body
+    )
+  )
 }
 
-export default App;
+
+
+
+
+
+
+function App(){
+  const [data,setData]=useState(false)
+
+ function togglefunction(){
+  setData(!data);
+ }
+return (
+  <div className='app'>
+    <button onClick={togglefunction}>Open Comment</button>
+    {data && <Box close={togglefunction}></Box>}
+  </div>
+ 
+
+)
+}
+
+export default App
